f5d4fe0facb280b4a41bc14bbba80f7480b76632
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2024-07-31 19:29:11 -0300

    feat: add the ORM Prisma for the project and add the first route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/hello', (request, reply) => {
-    return 'Hello NLW'
+app.post('/post', async (request, reply) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+    
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+
+    return reply.status(201).send({pollId: poll.id})
 })
 
 app.listen({ port: 3330 }).then(() => {
