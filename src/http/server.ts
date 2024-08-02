51244226cd796ab3329e13e13716b394905bab41
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2024-08-02 14:27:45 -0300

    feat: creating more infos for database and add a new routes for the project

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,10 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
+import { createPoll } from './routes/create-polls'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.post('/post', async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
+app.register(createPoll)
 
-    const { title } = createPollBody.parse(request.body)
-    
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    })
-
-    return reply.status(201).send({pollId: poll.id})
-})
-
-app.listen({ port: 3330 }).then(() => {
+app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
