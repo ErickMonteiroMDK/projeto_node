e762991128efd4a1e5f2b5a343d54d3efc66b0a4
ErickMonteiroMDK <erickmonteiromdk@gmail.com>
2024-08-02 14:56:39 -0300

    feat: add get and vote routes for the project

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
 import { createPoll } from './routes/create-polls'
+import { getPoll } from './routes/get-polls'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
